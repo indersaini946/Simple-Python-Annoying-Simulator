3131873f0868bca70f750ba5c211673dfac8a27f
Inder Saini <67489299+indersaini946@users.noreply.github.com>
2020-06-29 22:22:19 -0700

    Add files via upload

---
+++ Python #2/index.py
@@ -0,0 +1,10 @@
+print("Welcome to the annoying simulator.")
+print("How long can you survive.")
+print()
+def main():
+    answer = input("Why? ")
+    print()
+    print(answer)
+    main()
+
+main()
